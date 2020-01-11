88925b0972d38d8d24aaf741f791b770406dab7e
Anastasiyapash <nas.pash9@gmail.com>
2020-01-11 21:15:38 +0700

    validation examlist,question

--- app/models/examlist.rb
+++ app/models/examlist.rb
@@ -1,4 +1,9 @@
 class Examlist < ApplicationRecord
   belongs_to :student
   belongs_to :examination
+  validates :student, presence: true
+  validates :examination, presence: true
+  validates :mark, presence: true,
+                   numericality: { greater_than: 0,
+                                   less_than: 5 }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :discipline
+  validates :content, presence: true,
+                      length: { minimum: 3,
+                                maximum: 100 }
 end
