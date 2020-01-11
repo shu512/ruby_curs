7a2a6475e8205404de590430bcf3c15c1321ee75
Anastasiyapash <nas.pash9@gmail.com>
2020-01-11 20:46:23 +0700

    validation for discipline,examination

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,3 +1,6 @@
 class Discipline < ApplicationRecord
   has_many :questions, dependent: :destroy
+  validates :title, presence: true,
+                    length: { minimum: 3,
+                              maximum: 30 }
 end
--- app/models/examination.rb
+++ app/models/examination.rb
@@ -1,4 +1,7 @@
 class Examination < ApplicationRecord
   belongs_to :group
   belongs_to :discipline
+  validates :date, presence: true
+  validates :group, presence: true
+  validates :discipline, presence: true
 end
