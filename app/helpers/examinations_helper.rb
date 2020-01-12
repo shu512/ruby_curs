2ffc5883b3aca4c23fd6e37102b1f0f164a0cc14
shu512 <79231049479@yandex.ru>
2020-01-13 03:54:32 +0700

    small changes

--- app/helpers/examinations_helper.rb
+++ app/helpers/examinations_helper.rb
@@ -1,10 +1,4 @@
 module ExaminationsHelper
-  # return array: [[discipline_name, discipline_id], ..]
-  def disciplines_to_a
-    Discipline.all.map do |i|
-      [].push(i.title, i.id)
-    end
-  end
 
   def random_questions
     questions = @discipline.questions.map(&:content)
--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,8 +1,2 @@
 module StudentsHelper
-  # return array: [[group_name, group_id], ..]
-  def groups_to_a
-    Group.all.map do |i|
-      [].push(i.name, i.id)
-    end
-  end
 end
