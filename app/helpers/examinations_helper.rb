c5fe4ad696a42311e7beb4d1270c4cc866dae337
shu512 <79231049479@yandex.ru>
2020-01-17 19:56:38 +0700

    changes in views

--- app/helpers/examinations_helper.rb
+++ app/helpers/examinations_helper.rb
@@ -1,11 +1,10 @@
 module ExaminationsHelper
-
   def random_questions
     questions = @discipline.questions.map(&:content)
     selected_questions = []
     while selected_questions.size != 3
       rnd = rand(0...questions.size)
-      selected_questions.push(rnd) unless selected_questions.include? rnd
+      selected_questions.push(questions[rnd]) unless selected_questions.include? questions[rnd]
     end
     selected_questions
   end
